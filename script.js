7f60dc6a2865f736ee21430534be87a27c666309
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2022-12-18 10:24:05 +0530

    navbar fixed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const bodyEl = document.querySelector("body");
 const navBtn = document.querySelector(".navbar-toggler");
 const navEl = document.querySelector(".navbar");
+const navDiv = document.querySelector(".navbar-collapse");
 const navLinks = document.querySelectorAll(".nav-link");
 
 // Functions
@@ -21,6 +22,8 @@ const toggleNavColor = () => {
 navBtn.addEventListener("click", toggleNavColor);
 
 window.onscroll = function () {
+  if (navDiv.classList.contains("show")) return;
+
   if (
     document.body.scrollTop >= 150 ||
     document.documentElement.scrollTop >= 150
@@ -30,6 +33,7 @@ window.onscroll = function () {
       link.classList.add("text-nav");
     });
   } else {
+    // console.log(navDiv.classList.contains("show"));
     navEl.classList.remove("bg-nav");
     navLinks.forEach((link) => {
       link.classList.remove("text-nav");
